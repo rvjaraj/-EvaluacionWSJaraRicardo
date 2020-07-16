8866a39f39dd20cf1128241de14c8c344feb0b91
Ricardo Vinicio Jara jara <5940607-RicardoVinicioJara@users.noreply.gitlab.com>
2020-07-15 19:29:55 -0500

    soap listo

---
+++ EvaluacionWSJaraRicardo/src/main/java/ec/edu/ups/ws/business/services/SrvSoap.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.ws.business.services;
+
+import ec.edu.ups.ws.business.ServicesON;
+import ec.edu.ups.ws.emtitis.Amortizacion;
+import ec.edu.ups.ws.emtitis.Mensajes;
+import ec.edu.ups.ws.emtitis.PetisionSRV;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.inject.Inject;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author Vinicio
+ */
+@WebService(serviceName = "SrvSoap")
+public class SrvSoap {
+    @Inject
+    ServicesON servicesON;
+    
+    @WebMethod
+    public Mensajes solicitudCredito(PetisionSRV petisionSRV){
+        return servicesON.solicitudCredito(petisionSRV);
+    }
+    
+    @WebMethod
+    public List<Amortizacion> listaAmotizacion(String codigo){
+        try {
+            return servicesON.listaAmortizacio(codigo);
+        } catch (Exception ex) {
+            Logger.getLogger(SrvSoap.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+}
